f75ae558d7bcdc39412aa585575c77a711928cde
Mohamed Fathy <m.fathy@microtec.com.eg>
2024-02-05 11:01:44 +0200

    adjusting the code for the new prod variables

--- projects/bussiness-owners/src/environments/environment.development.ts
+++ projects/bussiness-owners/src/environments/environment.development.ts
@@ -5,11 +5,12 @@ export const environment = {
   ClientId: 'BussinessOwner',
   Platform: 'Web',
   openIdConfig: {
-    authority: 'http://192.168.100.191:8080',
-   redirectUrl: window.location.origin + '/bussinessowners/login-redirect',
-   postLogoutRedirectUri: window.location.origin + '/bussinessowners/logout-redirect',
+    authority: 'https://192.168.100.191:2000',
+    redirectUrl: window.location.origin + '/bussinessowners/login-redirect',
+    postLogoutRedirectUri:
+      window.location.origin + '/bussinessowners/logout-redirect',
     clientId: 'microtecadminfrontend',
-    scope: 'openid profile',
+    scope: 'openid profile email',
     responseType: 'code',
     silentRenew: true,
     useRefreshToken: true,
--- projects/bussiness-owners/src/environments/environment.ts
+++ projects/bussiness-owners/src/environments/environment.ts
@@ -1,16 +1,16 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://192.168.100.191:9001',
+  baseUrl: 'https://192.168.100.191:2003',
   Version: 'V1',
   ClientId: 'BussinessOwner',
   Platform: 'Web',
   openIdConfig: {
-    authority: 'https://192.168.100.191:8085',
+    authority: 'https://192.168.100.191:2000',
     redirectUrl: window.location.origin + '/bussinessowners/login-redirect',
     postLogoutRedirectUri:
       window.location.origin + '/bussinessowners/logout-redirect',
     clientId: 'microtecadminfrontend',
-    scope: 'openid profile',
+    scope: 'openid profile email',
     responseType: 'code',
     silentRenew: true,
     useRefreshToken: true,
