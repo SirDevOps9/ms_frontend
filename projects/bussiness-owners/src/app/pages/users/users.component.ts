092792af0e1af62db9564c62fd25920e96841012
Sandra Yasser <Sandra.yasser@microtec.com.eg>
2024-02-08 13:07:36 +0200

    translate messange to arabic

--- projects/bussiness-owners/src/app/pages/users/users.component.ts
+++ projects/bussiness-owners/src/app/pages/users/users.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit ,PipeTransform} from '@angular/core';
+import { Component, OnInit, PipeTransform } from '@angular/core';
 import { UserListResponse } from '../../models/users/userlist.response';
-import { RouterService, ToasterService } from 'shared-lib';
+import { ToasterService } from 'shared-lib';
 import { UserService } from '../../services/users.httpsservice';
 import { LanguageService } from 'dist/shared-lib';
 @Component({
@@ -10,42 +10,44 @@ import { LanguageService } from 'dist/shared-lib';
 })
 export class UsersComponent implements OnInit {
   userData: UserListResponse[];
-  
 
   constructor(
     public languageService: LanguageService,
     private toasterService: ToasterService,
-    private routerService: RouterService,
     private userService: UserService
   ) {}
   ngOnInit() {
     this.getAllUsers();
   }
-  getAllUsers(){
+  getAllUsers() {
     this.userService.getAll().subscribe({
       next: (res) => {
         this.userData = res;
       },
     });
   }
-  async ActivateAndDeactivate(id : number , currentstatus :Boolean){
-    const confirmed = await this.toasterService.showConfirm("ConfirmButtonTexttochangstatus");
-    let status ;
-    if (confirmed) 
-    {
+  async activateAndDeactivate(id: number, currentstatus: boolean) {
+    const confirmed = await this.toasterService.showConfirm(
+      'ConfirmButtonTexttochangstatus'
+    );
+    if (confirmed) {
       this.userService.ActivateAndDeactivate(id).subscribe({
-        next: (res) => {
-          if(currentstatus)this.toasterService.showSuccess('Success', this.languageService.transalte('User.userDeactivatedSuccessfully'));
+        next: () => {
+          if (currentstatus)
+            this.toasterService.showSuccess(
+              'Success',
+              this.languageService.transalte('User.UserDeactivatedSuccessfully')
+            );
           else
-          this.toasterService.showSuccess('Success', this.languageService.transalte('User.userActivatedSuccessfully'));
+            this.toasterService.showSuccess(
+              'Success',
+              this.languageService.transalte('User.UserActivatedSuccessfully')
+            );
 
-          let indexToChange = this.userData.findIndex(item => item.id === id);
-          this.userData[indexToChange].isActive = !currentstatus;
-            },
-            error: (err) => {
-              this.toasterService.showError('Error',err)
-            }
-      })
+          let indexToChange = this.userData.find((item) => item.id === id);
+          indexToChange!.isActive = !currentstatus;
+        },
+      });
     }
   }
 }
