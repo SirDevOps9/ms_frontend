fbc2a634186c47970bd3f24aaf7783c38d485b25
Rawan Abdelkader <Rawan.Abdelkader@microtec.com.eg>
2024-02-08 16:31:39 +0200

    commit

---
+++ projects/bussiness-owners/src/app/pages/company/add-compny/add-compny.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-compny',
+  templateUrl: './add-compny.component.html',
+  styleUrls: ['./add-compny.component.css']
+})
+export class AddCompnyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
