7c737535bd645724dfe17677a84f395c86d7021e
hager <hagar.mostafa@microtec.com.eg>
2024-02-17 20:02:45 +0200

    .

--- projects/bussiness-owners/src/app/pages/invite-current-user/invite-current-user.component.ts
+++ projects/bussiness-owners/src/app/pages/invite-current-user/invite-current-user.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {  FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { UserListResponse } from '../../models/users/userlist.response';
+import { UserService } from '../../services/users.httpsservice';
 
 @Component({
   selector: 'invite-current-user',
@@ -7,17 +9,26 @@ import {  FormGroup, Validators, FormBuilder } from '@angular/forms';
   styleUrls: ['./invite-current-user.component.css']
 })
 export class InviteCurrentUserComponent implements OnInit {
-
   userForm: FormGroup;
+  userData: UserListResponse[] = [];
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private userService: UserService) { }
 
   ngOnInit(): void {
     this.userForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]]
+      selectedOption: ['', Validators.required],
     });
+
+    this.getAllUsers();
   }
 
+  getAllUsers() {
+    this.userService.getAll().subscribe({
+      next: (res) => {
+        this.userData = res;
+      },
+    });
+  }
   onSubmit() {
     if (this.userForm.valid) {
       console.log('Form submitted successfully!');
