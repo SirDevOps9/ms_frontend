1a2c1fbbdad33c16b9885a3483f53a332530999f
hager <hagar.mostafa@microtec.com.eg>
2024-02-29 05:58:50 +0200

    cards html

--- projects/bussiness-owners/src/app/pages/Plan/Plan.component.ts
+++ projects/bussiness-owners/src/app/pages/Plan/Plan.component.ts
@@ -25,9 +25,9 @@ export class PlanComponent implements OnInit {
 
   ngOnInit() {
     this.plansList = [
-      { id: 14, name: 'plane 1' },
-      { id: 2, name: 'plane 2' },
-      { id: 3, name: 'plane 3' },
+      { id: 14, name: 'plane 1' ,subdomain:"subdomain1"},
+      { id: 2, name: 'plane 2' ,subdomain:"subdomain2"},
+      { id: 3, name: 'plane 3',subdomain:"subdomain3" },
     ];
   }
 
