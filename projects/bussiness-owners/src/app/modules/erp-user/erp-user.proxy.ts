198268e248c119c27496c26ffb9cfe04f10bbb10
Yasser Sallam <ysallam.c@solutions.com.sa>
2024-03-14 00:45:53 +0300

    add image thumbnail to image uploader

--- projects/bussiness-owners/src/app/modules/erp-user/erp-user.proxy.ts
+++ projects/bussiness-owners/src/app/modules/erp-user/erp-user.proxy.ts
@@ -1,22 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { APIResponse, BaseService } from 'shared-lib';
+import { APIResponse, HttpService } from 'shared-lib';
 import { UserListResponse } from '../user/models';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ERPUserProxy {
+  getAll(subscriptionId: number): Observable<APIResponse<UserListResponse[]>> {
+    return this.httpService.get<APIResponse<UserListResponse[]>>(
+      `ERPUser?subscriptionId=${subscriptionId}`
+    );
+  }
 
-
-getAll(subscriptionId: number): Observable<APIResponse<UserListResponse[]>> {
-  return this.baseService.get<APIResponse<UserListResponse[]>>(
-    `ERPUser?subscriptionId=${subscriptionId}`
-  );
-}
-
-
-constructor(private baseService: BaseService) { }
-
+  constructor(private httpService: HttpService) {}
 }
