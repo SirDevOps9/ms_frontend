6b3e1d2a05f71c19eb2110a1037fa15be6c4b2b8
Ahmed Gamal <Ahmed.Gamal@microtec.com.eg>
2024-03-14 15:49:27 +0200

    remove httpService

--- projects/bussiness-owners/src/app/modules/erp-user/erp-user.proxy.ts
+++ projects/bussiness-owners/src/app/modules/erp-user/erp-user.proxy.ts
@@ -8,7 +8,7 @@ import { UserListResponse } from '../user/models';
 })
 export class ERPUserProxy {
   getAll(subscriptionId: number): Observable<APIResponse<UserListResponse[]>> {
-    return this.httpService.get<APIResponse<UserListResponse[]>>(
+    return this.baseService.get<APIResponse<UserListResponse[]>>(
       `ERPUser?subscriptionId=${subscriptionId}`
     );
   }
