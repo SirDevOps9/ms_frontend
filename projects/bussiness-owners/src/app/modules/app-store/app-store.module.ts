cab945c5591c3d658e81590f28d16c71195877dc
Abdulrahman Almowafy <amowafy@appliedscienceint.com>
2024-03-26 17:20:01 +0200

    remove unused component

--- projects/bussiness-owners/src/app/modules/app-store/app-store.module.ts
+++ projects/bussiness-owners/src/app/modules/app-store/app-store.module.ts
@@ -1,12 +1,10 @@
 import { AuthGuard } from 'microtec-auth-lib';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ListAppsComponent } from './pages/list-apps/list-apps.component';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from '../layout/layout-page/layout.component';
 import { BreadcrumbLabel, SharedLibModule } from 'shared-lib';
 import { HttpClientModule } from '@angular/common/http';
-import { CardAppsComponent } from './pages/card-apps/card-apps.component';
 import { SelectSubdomainComponent } from './components/select-subdomain.component';
 import { AppDetailsComponent } from './pages/app-details/app-details.component';
 
@@ -23,14 +21,6 @@ const routes: Routes = [
           breadcrumb: BreadcrumbLabel.APP_STORE,
         },
       },
-      {
-        path: 'cards',
-        component: CardAppsComponent,
-        canActivate: [AuthGuard],
-        data: {
-          breadcrumb: BreadcrumbLabel.APP_STORE,
-        },
-      }
     ]
   }
 ]
@@ -38,7 +28,6 @@ const routes: Routes = [
 @NgModule({
   declarations: [
     ListAppsComponent,
-    CardAppsComponent,
     SelectSubdomainComponent,
     AppDetailsComponent
   ],
