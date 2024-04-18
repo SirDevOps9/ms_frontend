834cf2b0534b3e0be34b0b6c7803366cd4fa151f
Hager Mostafa <hagar.mostafa@microtec.com.eg>
2024-04-18 12:25:48 +0200

    invite user form controls

--- projects/bussiness-owners/src/app/modules/user/components/invite-form/user-invite-form.component.ts
+++ projects/bussiness-owners/src/app/modules/user/components/invite-form/user-invite-form.component.ts
@@ -1,19 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
 import {
-  DefaultExceptionModel,
-  FormsService,
-  LanguageService,
-  LoaderService,
-  LookupEnum,
-  LookupsService,
-  ToasterService,
-  customValidators,
-  lookupDto,
-} from 'shared-lib';
-import { UserService } from '../../user.service';
-import { InviteUserDto } from '../../models';
+  FormBuilder,
+  FormControl,
+  FormGroup,
+} from '@angular/forms';
+import { FormsService, customValidators } from 'shared-lib';
+import {
+  DialogService,
+  DynamicDialogConfig,
+  DynamicDialogRef,
+} from 'primeng/dynamicdialog';
 
 @Component({
   selector: 'app-user-invite-form',
@@ -21,61 +17,46 @@ import { InviteUserDto } from '../../models';
   styleUrls: ['./user-invite-form.component.scss'],
 })
 export class UserInviteFormComponent implements OnInit {
-  submitted = false;
   inviteForm: FormGroup;
-  lookups: { [key: string]: lookupDto[] };
-  LookupEnum = LookupEnum;
+
+
+
   ngOnInit() {
-    this.loadLookups();
-    this.initializeUserForm();
-    this.Subscribe();
-  }
-  Subscribe() {
-    this.lookupsService.lookups.subscribe((l) => (this.lookups = l));
+    this.initializesubDomainForm();
   }
 
-  onSubmit() {
-    if (!this.formService.validForm(this.inviteForm, true)) return;
-    const userModel: InviteUserDto = this.inviteForm.value;
-    // this.userService.inviteUser(userModel, this.ref);
-    this.userService.inviteUserPipe(userModel).subscribe({
-      next: (res) => {
-        this.toasterService.showSuccess(
-          this.languageService.transalte('User.Inviteform.Success'),
-          this.languageService.transalte('User.Inviteform.InviationSent')
-        );
-        this.loaderService.hide();
+  initializesubDomainForm() {
+    this.inviteForm = this.fb.group({
+      loginEmail: new FormControl('', customValidators.required,),
+      company: new FormControl('', customValidators.required,),
+      branches: new FormControl('', customValidators.required,),
+      domainSpace: new FormControl('', customValidators.required,),
+      selectLicence: new FormControl('', customValidators.required,),
 
-        this.ref.close(res);
-      },
     });
   }
-  loadLookups() {
-    this.lookupsService.loadLookups([
-      LookupEnum.BusinessRole,
-      LookupEnum.Subscription,
-    ]);
+
+
+  onSubmit() {
+    // if (!this.formService.validForm(this.subdomainForm, true)) return;
+    // const domainModel: AddDomainSpaceDto = {
+    //   ...this.subdomainForm.value,
+    //   purchasingPaymentPeriod: this.purchasingPaymentPeriod,
+    // };
+
+    // this.subscriptionService.addSubdomain(domainModel, this.ref);
   }
+
   onCancel() {
     this.ref.close();
   }
 
-  initializeUserForm() {
-    this.inviteForm = this.fb.group({
-      email: ['', [customValidators.required, customValidators.email]],
-      subscriptions: ['', customValidators.required],
-      bORoles: ['', customValidators.required],
-    });
-  }
+
   constructor(
+    public config: DynamicDialogConfig,
     public dialogService: DialogService,
     private fb: FormBuilder,
     private formService: FormsService,
-    private userService: UserService,
     private ref: DynamicDialogRef,
-    public lookupsService: LookupsService,
-    private toasterService: ToasterService,
-    private languageService: LanguageService,
-    private loaderService: LoaderService
   ) {}
 }
