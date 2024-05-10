737768ea457dada17a92baca8eb9e170576317bd
mfathydigitect <m.fathy@microtec.com.eg>
2024-05-10 21:47:05 +0300

    adding the new proxy for erp and manging the package.json

---
+++ erp-proxy.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const https = require('https');
+const fs = require('fs');
+
+const app = express();
+
+
+// Define your projects and their paths
+const projects = [
+  { path: '/erp', target: 'https://develop.localhost.com:4001' },
+  { path: '/accounting', target: 'https://develop.localhost.com:4002' },
+  { path: '/hr', target: 'https://develop.localhost.com:4003' },
+];
+
+projects.forEach(({ path, target }) => {
+  const proxyOptions = {
+    target,
+    changeOrigin: true,
+    secure: false, // Enable SSL
+    agent: https.globalAgent, // Use global agent
+   // ca: fs.readFileSync('ssl\\localhost.crt') // Provide the certificate authority
+  };
+  app.use(path, createProxyMiddleware(proxyOptions));
+});
+
+
+const defaultPath = projects.length > 0 ? projects[0].path : '/';
+app.get('/', (req, res) => res.redirect(defaultPath));
+
+// SSL certificate options
+const options = {
+  key: fs.readFileSync('ssl\\localhost.key'),
+  cert: fs.readFileSync('ssl\\localhost.crt'),
+};
+
+// Start the HTTPS server
+const port = 4000; // Default HTTPS port
+https.createServer(options, app).listen(port, 'develop.localhost.com', () => {
+  console.log(`Gateway server running on https://develop.localhost.com:${port}`);
+});
