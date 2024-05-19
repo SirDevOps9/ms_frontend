8f8e5e600fdf10acde95b3c2774b151120cd0597
Hager-Microtec Dev <Hager-Microtec Dev@MICROTEC-HAGERD>
2024-05-19 15:42:23 +0300

    style

--- projects/apps-hr/src/app/modules/employee/pages/create-employee/create-employee.component.ts
+++ projects/apps-hr/src/app/modules/employee/pages/create-employee/create-employee.component.ts
@@ -10,7 +10,7 @@ import {
 @Component({
   selector: 'app-create-employee',
   templateUrl: './create-employee.component.html',
-  styleUrls: ['./create-employee.component.css'],
+  styleUrls: ['./create-employee.component.scss'],
 })
 export class CreateEmployeeComponent implements OnInit {
   addEmployeeForm: FormGroup;
