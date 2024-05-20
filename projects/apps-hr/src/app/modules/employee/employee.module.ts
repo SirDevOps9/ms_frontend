80911af72c581baf3bb00eb33514ca98ed1ed001
hager <hagar.mostafa@microtec.com.eg>
2024-05-20 20:33:54 +0300

    view employee component

--- projects/apps-hr/src/app/modules/employee/employee.module.ts
+++ projects/apps-hr/src/app/modules/employee/employee.module.ts
@@ -7,6 +7,7 @@ import { AuthGuard } from 'microtec-auth-lib';
 import { NgModule } from '@angular/core';
 import { EmployeeListComponent } from './pages/employee-list/employee-list.component';
 import { CreateEmployeeComponent } from './pages/create-employee/create-employee.component';
+import { ViewEmployeeComponent } from './pages/view-employee/view-employee.component';
 
 const routes: Routes = [
   {
@@ -24,6 +25,14 @@ const routes: Routes = [
       {
         path: 'employee/add',
         component: CreateEmployeeComponent,
+        //canActivate: [AuthGuard],
+        data: {
+          breadcrumb: BreadcrumbLabel.EMPLOYEE_ADD,
+        },
+      },
+      {
+        path: 'employee/view',
+        component: ViewEmployeeComponent,
         //canActivate: [AuthGuard],
         data: {
           breadcrumb: BreadcrumbLabel.EMPLOYEE_ADD,
