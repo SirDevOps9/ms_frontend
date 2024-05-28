6689cfec2af46c2401cafe750ca48b6e4aa4e46e
Ahmed Gamal <Ahmed.Gamal@microtec.com.eg>
2024-05-28 16:43:14 +0300

    update

--- projects/apps-accounting/src/app/modules/account/components/add-chart/add-chart.component.ts
+++ projects/apps-accounting/src/app/modules/account/components/add-chart/add-chart.component.ts
@@ -52,10 +52,10 @@ export class AddChartComponent {
     private languageService: LanguageService
   ) {
     this.formGroup = formBuilder.group({
-      name: new FormControl('', customValidators.length(0, 255)),
+      name: new FormControl('', [customValidators.length(0, 255) , customValidators.required]),
       levelId: new FormControl(''),
-      accountCode: new FormControl('', customValidators.required),
-      parentId: new FormControl(''),
+      accountCode: new FormControl(''),
+      parentId: new FormControl(null),
       accountSectionName: new FormControl(''),
       natureId: new FormControl('', customValidators.required),
       hasNoChild: new FormControl(false),
