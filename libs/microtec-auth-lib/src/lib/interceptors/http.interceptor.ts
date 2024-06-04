bca9b643c86136b23b494e4556fbb368dec0b254
Mohamed Fathy <m.fathy@microtec.com.eg>
2024-06-04 15:21:14 +0300

    adjusting refresh token

--- libs/microtec-auth-lib/src/lib/interceptors/http.interceptor.ts
+++ libs/microtec-auth-lib/src/lib/interceptors/http.interceptor.ts
@@ -6,40 +6,33 @@ import {
   HttpEvent,
   HttpErrorResponse,
 } from '@angular/common/http';
-import { Observable, catchError, throwError } from 'rxjs';
-import {
-  HeaderParams,
-  RouterService,
-  StorageKeys,
-  StorageService,
-} from 'shared-lib';
+import { Observable, catchError, switchMap, throwError } from 'rxjs';
+import { HeaderParams, RouterService, StorageKeys, StorageService } from 'shared-lib';
+import { AuthService } from '../services';
+import { LoginResponse } from 'angular-auth-oidc-client';
 
 @Injectable()
 export class ERPInterceptor implements HttpInterceptor {
   constructor(
     private routerService: RouterService,
-    private localStorageService: StorageService
+    private localStorageService: StorageService,
+    private authService: AuthService
   ) {}
 
-  intercept(
-    request: HttpRequest<any>,
-    next: HttpHandler
-  ): Observable<HttpEvent<any>> {
-    //const clonedRequest = request.clone();
-    var tenantID = this.localStorageService.getItem(StorageKeys.TENANT);
-    const clonedRequest = request.clone({
-      headers: request.headers.append(
-        HeaderParams.TENANT_ID,
-        tenantID ? tenantID : ''
-      ),
-    });
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const clonedRequest = request.clone();
+    // var tenantID = this.localStorageService.getItem(StorageKeys.TENANT);
+    // const clonedRequest = request.clone({
+    //   headers: request.headers.append(HeaderParams.TENANT_ID, tenantID ? tenantID : ''),
+    // });
     return next.handle(clonedRequest).pipe(
       catchError((error: any) => {
         if (error instanceof HttpErrorResponse) {
           console.log('Interceptor Error', error);
           if (error.status === 401) {
-           // this.routerService.navigateTo('un-authorized');
-            this.routerService.navigateTo('login');
+            return this.handleUnAuthorizedError(clonedRequest, next);
+            // this.routerService.navigateTo('un-authorized');
+            //this.routerService.navigateTo('login');
           } else if (error.status === 403) {
             this.routerService.navigateTo('un-authorized');
           } else {
@@ -47,6 +40,24 @@ export class ERPInterceptor implements HttpInterceptor {
           }
         }
         return throwError(() => error);
+      })
+    );
+  }
+
+  private handleUnAuthorizedError(request: HttpRequest<unknown>, next: HttpHandler) {
+    return this.authService.refreshToken().pipe(
+      switchMap((res: LoginResponse) => {
+        const updatedRequest = request.clone({
+          setHeaders: {
+            Authorization: `Bearer ${res.accessToken}`,
+          },
+        });
+        return next.handle(updatedRequest);
+      }),
+      catchError((err) => {
+        // console.log('Int Error', err);
+        this.routerService.navigateTo('login');
+        return throwError(() => err);
       })
     );
   }
