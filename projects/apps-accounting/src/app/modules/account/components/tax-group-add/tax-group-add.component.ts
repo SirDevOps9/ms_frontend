b5008449869c67ad30b9c643b3cd130be3769d4f
Sandra Yasser <Sandra.yasser@microtec.com.eg>
2024-07-09 15:35:53 +0300

    tax group

--- projects/apps-accounting/src/app/modules/account/components/tax-group-add/tax-group-add.component.ts
+++ projects/apps-accounting/src/app/modules/account/components/tax-group-add/tax-group-add.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../../account.service';
 import { DynamicDialogRef } from 'primeng/dynamicdialog';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
-import { customValidators } from 'shared-lib';
+import { FormsService, customValidators } from 'shared-lib';
 import { AddTaxGroupDto } from '../../models';
 
 @Component({
@@ -17,6 +17,7 @@ export class TaxGroupAddComponent implements OnInit {
     private accountService: AccountService,
     private ref: DynamicDialogRef,
     private fb: FormBuilder,
+    private formsService: FormsService 
   
   ) {}
 
@@ -32,8 +33,8 @@ export class TaxGroupAddComponent implements OnInit {
   }
 
   save() {
-    if(!this.taxGroupForm.valid) return;
-    const taxGroupDto :AddTaxGroupDto=this.taxGroupForm.value;
+    if (!this.formsService.validForm(this.taxGroupForm, false)) return;
+        const taxGroupDto :AddTaxGroupDto=this.taxGroupForm.value;
     taxGroupDto.branchId="d69e6813-2646-41e7-a56c-538b7f91da39";
     taxGroupDto.companyId="98c91af6-16f4-477f-9b4a-db046a04b525";
     this.accountService.addTaxGroup(taxGroupDto,this.ref);
