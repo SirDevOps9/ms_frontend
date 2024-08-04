215af0e68fcc41fde66632fa18798e2368adfc99
Sandra Yasser <Sandra.yasser@microtec.com.eg>
2024-08-04 14:33:45 +0300

    edits

--- projects/apps-accounting/src/app/modules/account/account.service.ts
+++ projects/apps-accounting/src/app/modules/account/account.service.ts
@@ -279,7 +279,7 @@ export class AccountService {
       next: (res) => {
         this.toasterService.showSuccess(
           this.languageService.transalte('success'),
-          this.languageService.transalte('success')
+          this.languageService.transalte('TaxGroup.success')
         );
         this.loaderService.hide();
         dialogRef.close(res);
