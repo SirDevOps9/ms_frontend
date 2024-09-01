3975b674280833a0b784e6a22ea0885aab9880f5
Sandra Yasser <Sandra.yasser@microtec.com.eg>
2024-09-01 10:29:39 +0300

    edit

--- projects/apps-purchase/src/app/modules/purchase/pages/vendor-opening-balance/vendor-opening-balance-edit/vendor-opening-balance-edit.component.ts
+++ projects/apps-purchase/src/app/modules/purchase/pages/vendor-opening-balance/vendor-opening-balance-edit/vendor-opening-balance-edit.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { DropDownDto, JournalLineDropdownDto, lookupDto, LookupEnum } from '../../../models';
+import { DialogService } from 'primeng/dynamicdialog';
+import { customValidators, FormsService, LanguageService, LookupsService, RouterService, ToasterService } from 'shared-lib';
+import { Title } from '@angular/platform-browser';
+import { PurchaseService } from '../../../purchase.service';
+import { SharedPurchaseEnums } from '../../../models/sharedenums';
 
 @Component({
   selector: 'app-vendor-opening-balance-edit',
@@ -6,10 +13,231 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vendor-opening-balance-edit.component.scss']
 })
 export class VendorOpeningBalanceEditComponent implements OnInit {
+  formGroup: FormGroup;
+  vendorForm: FormArray;
+  openingJournalList: DropDownDto[];
+  linesDropDown: JournalLineDropdownDto[];
+  amount: string;
+  debitOrCredit: string;
+  openingJournalId: number;
+  vendorDropDownByAccountId: DropDownDto[] | any;
+  totalBalance: number;
+  LookupEnum = LookupEnum;
+  lookups: { [key: string]: lookupDto[] };
+  amountNature: string;
+  openingBalanceJournalEntryLineId: number;
 
-  constructor() { }
+  constructor(private fb: FormBuilder,
+    private dialog: DialogService,
+    private toasterService: ToasterService,
+    private languageService: LanguageService,
+    private formService: FormsService,
+    public routerService: RouterService,
+    private title: Title,
+    private lookupsService: LookupsService,
+    private purchaseService: PurchaseService,
+    public enums: SharedPurchaseEnums,) { }
 
   ngOnInit() {
+    this.initializeMainFormGroup();
+    this.loadLookups();
+    this.subscribe();
+  }
+
+  private initializeMainFormGroup(): void {
+    this.formGroup = this.fb.group({
+      OpeningJournal: new FormControl('', customValidators.required),
+      JournalLine: new FormControl('', customValidators.required),
+      amount: '',
+      amountNature: '',
+    });
+
+    this.vendorForm = this.fb.array([this.vendorLineFormGroup()]);
+  }
+
+  vendorLineFormGroup(): FormGroup {
+    return this.fb.group({
+      id: 0,
+      vendorId: new FormControl('', customValidators.required),
+      accountName: new FormControl(),
+      vendorCode: new FormControl(),
+      balance: new FormControl(0, [
+        customValidators.required,
+        customValidators.number,
+        customValidators.hasSpaces,
+        customValidators.nonZero,
+      ]),
+      balanceType: new FormControl('', [customValidators.required]),
+      displayName: new FormControl(),
+      dueDates: new FormControl([]),
+    });
+  }
+
+  subscribe() {
+    this.lookupsService.lookups.subscribe((l) => (this.lookups = l));
+
+    this.purchaseService.openingBalanceJournalEntryDropdownDataObservable.subscribe((res) => {
+      this.openingJournalList = res;
+    });
+    this.purchaseService.VendorDropDownByAccountIdObservable.subscribe((res) => {
+      this.vendorDropDownByAccountId = res;
+    });
+    this.purchaseService.JournalLinesDropDownDataObservable.subscribe((res: any) => {
+      this.linesDropDown = res;
+    });
+  }
+
+  cancel() {
+    this.routerService.navigateTo('/masterdata/customer-opening-balance');
+  }
+
+  openingBalanceJournalEntryDropdown() {
+    this.purchaseService.openingBalanceJournalEntryDropdown();
+  }
+
+  onOpeningJournalChange(event: any) {
+    this.getLinesDropDown(event);
+  }
+
+  getLinesDropDown(id: number) {
+    this.openingJournalId = id;
+    this.purchaseService.getLinesDropDown(id);
+  }
+  
+  public get items(): FormArray {
+    return this.vendorForm as FormArray;
+  }
+
+  addLine() {
+    if (!this.formService.validForm(this.vendorForm, false)) return;
+
+    this.items.push(this.vendorLineFormGroup());
+    this.vendorForm.updateValueAndValidity();
+  }
+
+  removeByFront(index: number) {
+    this.vendorForm.removeAt(index);
+    this.calculateTotalBalance();
+  }
+
+  loadLookups() {
+    this.lookupsService.loadLookups([LookupEnum.AccountNature]);
+  }
+  
+  openDistribute(data: any, account: number, index: number, VendorGroup: FormGroup) {
+    if (!this.formService.validForm(this.vendorForm, false)) return;
+
+    // if (data.balanceType != this.enums.BalanceType.Debit) {
+    //   this.toasterService.showError(
+    //     this.languageService.transalte('Error'),
+    //     this.languageService.transalte('Distribution')
+    //   );
+    //   return;
+    // } else {
+      // const ref = this.dialog.open(CustomerOpeningBalanceDistributeComponent, {
+      //   width: '750px',
+      //   height: '600px',
+      //   data: data,
+      // });
+    //   ref.onClose.subscribe((res) => {
+    //     if (res) {
+    //       data.dueDates = res;
+    //     } else {
+    //       data.dueDates = [];
+    //     }
+    //   });
+    // }
+  }
+
+  balanceTypeSelected(event: any, index: number) {
+    const line = this.items.at(index);
+    if (!line) {
+      return;
+    }
+
+    const selectedVendorId = line.get('vendorId')?.value;
+
+    if (!selectedVendorId) {
+      return;
+    }
+
+    const isVendorAlreadySelected = this.items.controls.some((group, i) => {
+      return group.get('vendorId')?.value === selectedVendorId && i !== index;
+    });
+
+    if (isVendorAlreadySelected) {
+      const matchingGroup = this.items.controls.find(
+        (group) => group.get('vendorId')?.value === selectedVendorId
+      );
+      const existingBalanceType = matchingGroup?.get('balanceType')?.value;
+
+      if (existingBalanceType && existingBalanceType !== event) {
+        this.toasterService.showError(
+          this.languageService.transalte('Error'),
+          this.languageService.transalte('openeingBalance.natureMismatchWithDuplicateCustomer')
+        );
+        line.get('balanceType')?.reset();
+        return;
+      }
+    }
+
+    line.get('balanceType')?.setValue(event);
+  }
+
+  calculateTotalBalance() {
+    this.totalBalance = this.vendorForm.controls.reduce((acc, control) => {
+      const debitValue = parseFloat(control.get('balance')?.value) || 0;
+      return acc + debitValue;
+    }, 0);
+  }
+
+  accountSelected(event: any, index: number) {
+    const line = this.items.at(index);
+    if (!line) {
+      return;
+    }
+
+    const selectedvendor = this.vendorDropDownByAccountId.find((c: any) => c.id === event);
+
+    if (!selectedvendor) {
+      return;
+    }
+
+    line.get('accountName')?.setValue(selectedvendor.name);
+    line.get('vendorCode')?.setValue(selectedvendor.code);
+    line.get('displayName')?.setValue(`${selectedvendor.code}`);
+
+    line.get('vendorId')?.setValue(selectedvendor.id);
+  }
+
+  onLinesChange(event: any) {
+    setTimeout(() => {
+      this.linesDropDown?.forEach((element: any) => {
+        if (element.id == event) {
+          this.formGroup.patchValue({
+            amount: element.amount,
+            amountNature: element.amountNature,
+          });
+          this.vendorDropDownByAccountId(element.accountId);
+          this.openingBalanceJournalEntryLineId = element.id;
+          this.amountNature = element.amountNature;
+        }
+      });
+    }, 500);
+  }
+  onSubmit(){
+    if (!this.formService.validForm(this.vendorForm, false)) return;
+
+    this.vendorForm.updateValueAndValidity();
+    this.formGroup.updateValueAndValidity();
+    const body = {
+      id: this.routerService.currentId,
+      openingBalanceJournalEntryLineId: this.openingBalanceJournalEntryLineId,
+      amountNature: this.amountNature,
+      customerOpeningBalanceDetails: this.items.value,
+    };
+    this.purchaseService.EditVendorOpeningBalance(body);
+
   }
 
 }
