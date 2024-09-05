ac8758b7b60aae40ae4bbe20ac1d059597cfa2c4
Amr Yousry <osary01@gmail.com>
2024-09-05 13:03:46 +0300

    purchase

--- projects/erp-home/src/app/modules/home/pages/dashboard/dashboard.component.ts
+++ projects/erp-home/src/app/modules/home/pages/dashboard/dashboard.component.ts
@@ -45,7 +45,7 @@ export class DashboardComponent implements OnInit {
     } else if (key === Modules.Finance) {
       location.href = '../finance';
     } else if (key === Modules.Purchase) {
-      location.href = '../inventory';
+      location.href = '../purchase';
     }else if (key === Modules.inventory) {
       location.href = '../inventory';
     }
