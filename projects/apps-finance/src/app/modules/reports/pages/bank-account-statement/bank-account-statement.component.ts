563cd881e065233e9328b2a056fb2107529247c2
hager <hagar.mostafa@microtec.com.eg>
2024-09-11 14:29:31 +0300

    .

--- projects/apps-finance/src/app/modules/reports/pages/bank-account-statement/bank-account-statement.component.ts
+++ projects/apps-finance/src/app/modules/reports/pages/bank-account-statement/bank-account-statement.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
 import { Title } from '@angular/platform-browser';
-import {  Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { GeneralService } from 'libs/shared-lib/src/lib/services/general.service';
 import {
   ToasterService,
@@ -26,12 +26,10 @@ import { SourceDocument } from '../../models/source-document-dto';
 })
 export class BankAccountStatementComponent {
   accountStatementForm: FormGroup;
-  defoultSelectedAcounts: number[] = [];
   tableData: BankAccountStatementDto;
   transactions: BankAccountStatementLinesDto[] = [];
   totalDebit: number;
   totalCredit: number;
-  cols: any[] = [];
   total: number = 0;
   currency: string;
   BankDropDown: DropDownDto[] = [];
@@ -41,26 +39,27 @@ export class BankAccountStatementComponent {
   selectedBankAccountName: string = '';
   openingDebit: number;
   openingCredit: number;
-  openingBalance:number;
-  openingHeader:String;
+  openingBalance: number;
+  openingHeader: String;
   constructor(
     private fb: FormBuilder,
     private reportsService: ReportsService,
-    private router:Router,
+    private router: Router,
     private titleService: Title,
     private languageService: LanguageService,
     private ToasterService: ToasterService,
     private PrintService: PrintService,
     public generalService: GeneralService,
-    private formsService: FormsService,
-
+    private formsService: FormsService
   ) {}
 
   ngOnInit() {
-    this.titleService.setTitle(this.languageService.transalte('BankAccountStatement.BankAccountStatement'));
+    this.titleService.setTitle(
+      this.languageService.transalte('BankAccountStatement.BankAccountStatement')
+    );
 
     this.initializeForm();
-    //this.initializeDates();
+    this.initializeDates();
 
     this.accountStatementForm.valueChanges.subscribe(() => {
       this.tableData = {} as BankAccountStatementDto;
@@ -99,7 +98,7 @@ export class BankAccountStatementComponent {
           this.openingDebit = res?.openingBalance?.debit;
           this.openingCredit = res?.openingBalance?.credit;
           this.openingBalance = res?.openingBalance?.balance;
-          this.openingHeader =res?.openingBalance?.headerDescription;
+          this.openingHeader = res?.openingBalance?.headerDescription;
         }
       );
     } else {
@@ -111,9 +110,11 @@ export class BankAccountStatementComponent {
   }
   initializeDates() {
     const today = new Date();
+    let startOfMonth = new Date(today.getFullYear(), today.getMonth(), 1);
+    startOfMonth.setDate(startOfMonth.getDate() + 1);
     const endOfMonth = new Date(today.getFullYear(), today.getMonth() + 1);
     this.accountStatementForm.patchValue({
-      dateFrom: today.toISOString().split('T')[0],
+      dateFrom: startOfMonth.toISOString().split('T')[0],
       dateTo: endOfMonth.toISOString().split('T')[0],
     });
   }
@@ -144,29 +145,27 @@ export class BankAccountStatementComponent {
     });
   }
 
-  routeToPaymentView(id:number, paymentname: string){
-
-    const test =location.href.split("/")
-        console.log(test[3]);
-        if(paymentname==SourceDocument.PaymentIn)
-          {
-            const url = this.router.serializeUrl(
-              this.router.createUrlTree([`${test[3]}/transcations/paymentin/view/${id}`])
-            );
-            window.open(url, '_blank');
-          }else{
-            const url = this.router.serializeUrl(
-              this.router.createUrlTree([`${test[3]}/transcations/paymentout/view/${id}`])
-            );
-    window.open(url, '_blank');
-      }
-  }
-      routeToJournalView(id:number){
-        const test =location.href.split("/")
-            console.log(test[3]);
-        const url = this.router.serializeUrl(
-          this.router.createUrlTree([`/accounting/transcations/journalentry/view/${id}`])
-        );
-        window.open(url, '_blank');
-          }
+  routeToPaymentView(id: number, paymentname: string) {
+    const test = location.href.split('/');
+    console.log(test[3]);
+    if (paymentname == SourceDocument.PaymentIn) {
+      const url = this.router.serializeUrl(
+        this.router.createUrlTree([`${test[3]}/transcations/paymentin/view/${id}`])
+      );
+      window.open(url, '_blank');
+    } else {
+      const url = this.router.serializeUrl(
+        this.router.createUrlTree([`${test[3]}/transcations/paymentout/view/${id}`])
+      );
+      window.open(url, '_blank');
     }
+  }
+  routeToJournalView(id: number) {
+    const test = location.href.split('/');
+    console.log(test[3]);
+    const url = this.router.serializeUrl(
+      this.router.createUrlTree([`/accounting/transcations/journalentry/view/${id}`])
+    );
+    window.open(url, '_blank');
+  }
+}
--- projects/apps-finance/src/app/modules/reports/pages/treasury-statement/treasury-statement.component.ts
+++ projects/apps-finance/src/app/modules/reports/pages/treasury-statement/treasury-statement.component.ts
@@ -2,8 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Title } from '@angular/platform-browser';
 import { ActivatedRoute, Router } from '@angular/router';
-import { customValidators, FormsService, LanguageService,  PrintService ,ToasterService } from 'shared-lib';
-import { treasuryStatementDto, TreasuryStatementfilterDto, TreasuryStatmentTransactionDto } from '../../models';
+import {
+  customValidators,
+  FormsService,
+  LanguageService,
+  PrintService,
+  ToasterService,
+} from 'shared-lib';
+import {
+  treasuryStatementDto,
+  TreasuryStatementfilterDto,
+  TreasuryStatmentTransactionDto,
+} from '../../models';
 import { TreasuryDropDown } from '../../../finance/models';
 import { TranscationsService } from '../../../transcations/transcations.service';
 import { ReportsService } from '../../reports.service';
@@ -13,26 +23,22 @@ import { SourceDocument } from '../../models/source-document-dto';
 @Component({
   selector: 'app-treasury-statement',
   templateUrl: './treasury-statement.component.html',
-  styleUrls: ['./treasury-statement.component.scss']
+  styleUrls: ['./treasury-statement.component.scss'],
 })
 export class TreasuryStatementComponent implements OnInit {
-
   reportForm: FormGroup;
-  treasuryDropDown: TreasuryDropDown[] = []
-  defoultSelectedAcounts: number[] = [];
+  treasuryDropDown: TreasuryDropDown[] = [];
   currency: string;
-  tableData: treasuryStatementDto ; 
-  cols: any[] = [];
+  tableData: treasuryStatementDto;
   total: number = 0;
   selectedTreasuryName: string = '';
   transactions: TreasuryStatmentTransactionDto[] = [];
   openingDebit: number;
   openingCredit: number;
-  openingBalance:number;
+  openingBalance: number;
   totalDebit: number;
   totalCredit: number;
   totalBalance: number;
-
 
   constructor(
     private fb: FormBuilder,
@@ -43,53 +49,50 @@ export class TreasuryStatementComponent implements OnInit {
     private ReportService: ReportsService,
     public generalService: GeneralService,
     private formsService: FormsService,
-    private router:Router,
+    private router: Router,
     private ToasterService: ToasterService
-
-
-
   ) {}
 
   ngOnInit() {
-    this.titleService.setTitle(this.languageService.transalte('TreasuryStatement.TreasuryStatement'));
+    this.titleService.setTitle(
+      this.languageService.transalte('TreasuryStatement.TreasuryStatement')
+    );
     this.initializeForm();
     this.getTreasuryDropDown();
     this.initializeDates();
-     this.reportForm.valueChanges.subscribe(() => {
+    this.reportForm.valueChanges.subscribe(() => {
       this.tableData = {} as treasuryStatementDto;
-     });
+    });
 
-    this.ReportService.treasuryStatementObservable.subscribe(res => {
-      
+    this.ReportService.treasuryStatementObservable.subscribe((res) => {
       if (res && res.transactions && res.transactions.length > 0) {
         this.tableData = res;
-      this.transactions = res?.transactions
-      this.totalDebit = res?.totalDebit;
-      this.totalCredit = res?.totalCredit;
-      this.totalBalance = res?.totalBalance;
-      this.openingDebit = res?.openingBalanceDebit;
-      this.openingCredit = res?.openingBalanceCredit;
-      this.openingBalance = res?.openingBalanceBalance;
+        this.transactions = res?.transactions;
+        this.totalDebit = res?.totalDebit;
+        this.totalCredit = res?.totalCredit;
+        this.totalBalance = res?.totalBalance;
+        this.openingDebit = res?.openingBalanceDebit;
+        this.openingCredit = res?.openingBalanceCredit;
+        this.openingBalance = res?.openingBalanceBalance;
       }
-    })
+    });
 
-    this.reportForm.get('treasuryId')!.valueChanges.subscribe(Id => {
-      const selected = this.treasuryDropDown.find(x => x.id === Id);
+    this.reportForm.get('treasuryId')!.valueChanges.subscribe((Id) => {
+      const selected = this.treasuryDropDown.find((x) => x.id === Id);
       if (selected) {
-        console.log(" this.tableData ", this.tableData )
+        console.log(' this.tableData ', this.tableData);
         this.reportForm.get('currency')!.setValue(selected.currencyName);
-        this.currency=selected.currencyName
-        this.selectedTreasuryName = selected.name
-
+        this.currency = selected.currencyName;
+        this.selectedTreasuryName = selected.name;
       }
     });
   }
 
   getTreasuryDropDown() {
-    this.financeService.treasuryDropDown()
+    this.financeService.treasuryDropDown();
     this.financeService.getTreasuryDropDownDataObservable.subscribe((res: any) => {
-      this.treasuryDropDown = res  
-     })
+      this.treasuryDropDown = res;
+    });
   }
 
   initializeForm() {
@@ -97,15 +100,20 @@ export class TreasuryStatementComponent implements OnInit {
       dateFrom: new FormControl('', [customValidators.required]),
       dateTo: new FormControl('', [customValidators.required]),
       treasuryId: new FormControl('', [customValidators.required]),
-      currency:new FormControl('')
+      currency: new FormControl(''),
     });
   }
 
   initializeDates() {
     const today = new Date();
+    let startOfMonth = new Date(today.getFullYear(), today.getMonth(), 1);
+
+    startOfMonth.setDate(startOfMonth.getDate() + 1);
+
     const endOfMonth = new Date(today.getFullYear(), today.getMonth() + 1);
+
     this.reportForm.patchValue({
-      dateFrom: today.toISOString().split('T')[0],
+      dateFrom: startOfMonth.toISOString().split('T')[0],
       dateTo: endOfMonth.toISOString().split('T')[0],
     });
   }
@@ -113,13 +121,9 @@ export class TreasuryStatementComponent implements OnInit {
   printTable(id: string) {
     this.PrintService.print(id);
   }
-  getReportData(){
-
+  getReportData() {
     if (!this.formsService.validForm(this.reportForm, false)) return;
-    if (
-      this.reportForm.get('dateFrom')?.value >
-      this.reportForm.get('dateTo')?.value
-    ) {
+    if (this.reportForm.get('dateFrom')?.value > this.reportForm.get('dateTo')?.value) {
       this.ToasterService.showError(
         this.languageService.transalte('Error'),
         this.languageService.transalte('DateFromLessThanToValidation')
@@ -128,41 +132,42 @@ export class TreasuryStatementComponent implements OnInit {
 
     const formValue = this.reportForm.value;
     const filterDto: TreasuryStatementfilterDto = {
-    DateFrom: formValue.dateFrom,
-    DateTo: formValue.dateTo,
-    TreasuryId: formValue.treasuryId
-  };
-    this.ReportService.getTreasuryStatement(filterDto)
-    this.ReportService.treasuryStatementObservable.subscribe(data => {
+      DateFrom: formValue.dateFrom,
+      DateTo: formValue.dateTo,
+      TreasuryId: formValue.treasuryId,
+    };
+    this.ReportService.getTreasuryStatement(filterDto);
+    this.ReportService.treasuryStatementObservable.subscribe((data) => {
       this.tableData = data;
-    })
+    });
   }
 
-  routeToPaymentView(transaction:TreasuryStatmentTransactionDto){
-    const test =location.href.split("/")
+  routeToPaymentView(transaction: TreasuryStatmentTransactionDto) {
+    const test = location.href.split('/');
 
-        if(transaction.paymentName==SourceDocument.PaymentIn)
-        {
-          const url = this.router.serializeUrl(
-            this.router.createUrlTree([`${test[3]}/transcations/paymentin/view/${transaction.paymentInHeaderId}`])
-          );
-          window.open(url, '_blank');
+    if (transaction.paymentName == SourceDocument.PaymentIn) {
+      const url = this.router.serializeUrl(
+        this.router.createUrlTree([
+          `${test[3]}/transcations/paymentin/view/${transaction.paymentInHeaderId}`,
+        ])
+      );
+      window.open(url, '_blank');
+    } else {
+      const url = this.router.serializeUrl(
+        this.router.createUrlTree([
+          `${test[3]}/transcations/paymentout/view/${transaction.paymentOutHeaderId}`,
+        ])
+      );
+      window.open(url, '_blank');
+    }
+  }
 
-        }else{
-          const url = this.router.serializeUrl(
-            this.router.createUrlTree([`${test[3]}/transcations/paymentout/view/${transaction.paymentOutHeaderId}`])
-          );
-          window.open(url, '_blank');
-
-        }
-      }
-    
-      routeToJournalView(id:number){
-        const test =location.href.split("/")
-            console.log(test[3]);
-        const url = this.router.serializeUrl(
-          this.router.createUrlTree([`/accounting/transcations/journalentry/view/${id}`])
-        );
-        window.open(url, '_blank');
-          }
+  routeToJournalView(id: number) {
+    const test = location.href.split('/');
+    console.log(test[3]);
+    const url = this.router.serializeUrl(
+      this.router.createUrlTree([`/accounting/transcations/journalentry/view/${id}`])
+    );
+    window.open(url, '_blank');
+  }
 }
