39b7aae444039dd8bd9a47865d3234d20edbeb97
Hager-Microtec Dev <Hager-Microtec Dev@MICROTEC-HAGERD>
2024-09-16 12:43:10 +0300

    .

--- projects/apps-finance/src/app/modules/finance/finance-proxy.service.ts
+++ projects/apps-finance/src/app/modules/finance/finance-proxy.service.ts
@@ -211,6 +211,8 @@ export class FinanceProxyService {
   GetAccountBalance(id: number): Observable<number> {
     return this.httpService.get(`Bank/GetAccountBalance/${id}`);
   }
+
+  
   getAccountsHasNoChildren(
     quieries: string,
     pageInfo: PageInfo
