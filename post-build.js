7aa1e782fdeaeb1c959d3123f9224a28c1ce53f0
Mohamed Fathy <mohamedfathy234@gmail.com>
2024-10-08 13:19:08 +0300

    adding build date to each main js

--- post-build.js
+++ post-build.js
@@ -3,6 +3,41 @@ const path = require('path');
 const fse = require('fs-extra');
 
 // Get the output directory and the target directory from environment variables
+const currentDate = new Date().toLocaleString();
+const debugMessage = `console.log('Build Date: ${currentDate}');`;
+
+function addConsoleDebugToJsFiles(dir) {
+    fs.readdir(dir, (err, files) => {
+        if (err) {
+            console.error('Error reading directory:', dir, err);
+            return;
+        }
+
+        files.forEach(file => {
+            // Check if the file ends with .js and contains 'main' in the filename
+            if (file.endsWith('.js') && file.includes('main')) {
+                const jsFile = path.join(dir, file);
+
+                fs.access(jsFile, fs.constants.F_OK, (err) => {
+                    if (err) {
+                        console.error('JavaScript file not found:', jsFile);
+                        return;
+                    }
+
+                    // Append console.debug to the end of the JavaScript file
+                    fs.appendFile(jsFile, `\n${debugMessage}\n`, (err) => {
+                        if (err) {
+                            console.error('Error writing to file:', jsFile, err);
+                        } else {
+                            console.log(`Appended console.debug to: ${jsFile}`);
+                        }
+                    });
+                });
+            }
+        });
+    });
+}
+
 
 const publishPath = 'C:Users/tuy_2/Desktop/Publish';
 
@@ -23,3 +58,4 @@ function moveBuild() {
 
 // Execute the move
 moveBuild();
+addConsoleDebugToJsFiles(targetDir);
