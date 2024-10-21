e1daef7aa21aecb9425480e05a2be28a7080248a
Hager-Microtec Dev <Hager-Microtec Dev@MICROTEC-HAGERD>
2024-10-21 17:26:06 +0300

    .

--- projects/apps-sales/src/app/modules/sales/components/multi-select-items/multi-select-items.component.ts
+++ projects/apps-sales/src/app/modules/sales/components/multi-select-items/multi-select-items.component.ts
@@ -95,7 +95,32 @@ console.log("Item",x);
 
     });
 
-
+    // buildQuery(): string {
+    //   // Get the current values from the filter form
+    //   const isStorable = this.filterForm.get('isStorable')?.value;
+    //   const isService = this.filterForm.get('isService')?.value;
+    //   const hasExpiryDate = this.filterForm.get('hasExpiryDate')?.value;
+    
+    //   // Initialize an array to hold the query parameters
+    //   const query: string[] = [];
+    
+    //   // Add query parameters based on checkbox values
+    //   if (isStorable) {
+    //     query.push(`${this.sharedEnums.GetItemsQueryEnum.IsStorable}=${this.sharedEnums.GetItemsQueryEnum.IsStorable}`);
+    //   }
+    //   if (isService) {
+    //     query.push(`${this.sharedEnums.GetItemsQueryEnum.IsService}=${this.sharedEnums.GetItemsQueryEnum.IsService}`);
+    //   }
+    //   if (hasExpiryDate) {
+    //     query.push(`${this.sharedEnums.GetItemsQueryEnum.HasExpiryDate}=true`); // Adjust based on how your API expects this
+    //   }
+    
+    //   // Join the query parameters into a single string
+    //   const result = query.join('&');
+    //   console.log('Generated Query:', result);
+    
+    //   return result;
+    // }
 
     const result  = query.join('&')
     console.log('result', result)
