af909f08a5b00d4cd7a7d1e81b3c9c8de5a539fe
ahmed.saad@doctor-code.net <ahmed.msaad1516@gmail.com>
2024-11-12 10:45:41 +0200

    push-update

--- projects/apps-inventory/src/app/modules/items/items.service.ts
+++ projects/apps-inventory/src/app/modules/items/items.service.ts
@@ -8,7 +8,7 @@ import {
   RouterService,
   ToasterService,
 } from 'shared-lib';
-import { BehaviorSubject, map, ReplaySubject, Subject } from 'rxjs';
+import { BehaviorSubject, firstValueFrom, map, ReplaySubject, Subject } from 'rxjs';
 import {
   addBarcode,
   AddGeneralDto,
@@ -96,9 +96,18 @@ export class ItemsService {
   public stockOutDataSource = new BehaviorSubject<StockOutDto[]>([]);
 
   stockOutDataSourceeObservable = this.stockOutDataSource.asObservable();
+
+  public stockInDataSource = new BehaviorSubject<StockOutDto[]>([]);
+
+  stockInDataSourceeObservable = this.stockInDataSource.asObservable();
+
   public exportStockOutListDataSource = new BehaviorSubject<StockOutDto[]>([]);
 
   exportStockOutListDataSourceObservable = this.exportStockOutListDataSource.asObservable();
+
+
+  public exportStockInListDataSource = new BehaviorSubject<StockInDto[]>([]);
+  exportStockInListDataSourceObservable = this.exportStockInListDataSource.asObservable();
 
   saveItemDefGeneral = new BehaviorSubject<AddGeneralDto>({} as AddGeneralDto);
   saveItemDefGeneral$ = this.saveItemDefGeneral.asObservable()
@@ -352,22 +361,26 @@ this.itemProxy.getItemBarcodeById(id).subscribe({
   }
 
   async deleteStockIn(id: number) {
-    const confirmed = await this.toasterService.showConfirm(
-      this.languageService.transalte('ConfirmButtonTexttodelete')
-    );
-    if (confirmed) {
-      this.itemProxy.deleteStockIn(id).subscribe({
-        next: (res) => {
-          this.toasterService.showSuccess(
-            this.languageService.transalte('transactions.success'),
-            this.languageService.transalte('transactions.deleteStockIn')
-          );
+    try {
+      const confirmed = await this.toasterService.showConfirm(
+        this.languageService.transalte('ConfirmButtonTexttodelete')
+      );
 
-          const currentCostCenter = this.sendStockInDataSources.getValue();
-          const updatedCostCenter = currentCostCenter.filter((c) => c.id !== id);
-          this.sendStockInDataSources.next(updatedCostCenter);
-        },
-      });
+      if (confirmed) {
+        await firstValueFrom(this.itemProxy.deleteStockIn(id));
+
+        // Show success message
+        this.toasterService.showSuccess(
+          this.languageService.transalte('transactions.success'),
+          this.languageService.transalte('transactions.deleteStockIn')
+        );
+
+        const currentCostCenter = this.sendStockInDataSources.getValue();
+        const updatedCostCenter = currentCostCenter.filter((c) => c.id !== id);
+        this.sendStockInDataSources.next(updatedCostCenter);
+      }
+    } catch (error) {
+
     }
   }
   async deleteStockOut(id: number) {
@@ -1514,6 +1527,13 @@ getAllStockOut(quieries: string, pageInfo: PageInfo) {
     this.currentPageInfo.next(response.pageInfoResult);
   });
 }
+
+getAllStockIn(quieries: string, pageInfo: PageInfo) {
+  this.itemProxy.getAllStockIn(quieries, pageInfo).subscribe((response) => {
+    this.stockInDataSource.next(response.result);
+    this.currentPageInfo.next(response.pageInfoResult);
+  });
+}
 exportStockOutList(searchTerm?: string ,SortBy?:number,SortColumn?:string) {
   this.itemProxy.exportStockOutList(searchTerm ,SortBy,SortColumn).subscribe({
     next: (res: any) => {
@@ -1521,5 +1541,14 @@ exportStockOutList(searchTerm?: string ,SortBy?:number,SortColumn?:string) {
     },
   });
 }
+
+exportStockInList(searchTerm?: string ,SortBy?:number,SortColumn?:string) {
+  this.itemProxy.exportStockInList(searchTerm ,SortBy,SortColumn).subscribe({
+    next: (res: any) => {
+      this.exportStockInListDataSource.next(res);
+    },
+  });
+}
+
 }
 
