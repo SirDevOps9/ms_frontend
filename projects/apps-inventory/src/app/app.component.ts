e47d72eb3d06f373d0a0f7bbd58af9c0d641d380
Mohamed Fathy <m.fathy@microtec.com.eg>
2024-11-16 21:48:30 +0200

    adjust dev for ocelot and fix inventory

--- projects/apps-inventory/src/app/app.component.ts
+++ projects/apps-inventory/src/app/app.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { LanguageService, TitleService } from 'shared-lib';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  constructor(public languageService: LanguageService, private titleService: TitleService) {
+    this.languageService.setLang();
+  }
+  ngOnInit() {
+    this.titleService.setTitleFromRoute();
+  }
 }
