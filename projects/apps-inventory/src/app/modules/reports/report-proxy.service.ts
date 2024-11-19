ae47afdd30c5eaec3db9dee20ee8b71c4828f8b0
Bishoy Saeed <Bishoy@microtec.com>
2024-11-19 17:00:58 +0200

    +

--- projects/apps-inventory/src/app/modules/reports/report-proxy.service.ts
+++ projects/apps-inventory/src/app/modules/reports/report-proxy.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpService, PaginationVm } from 'shared-lib';
 import { WarehousesTables } from './models';
+import { LatestItems } from '../items/models';
 
 @Injectable({
   providedIn: 'root',
@@ -16,5 +17,11 @@ export class ReportProxyService {
       queryList += `?${query}`;
     }
     return this.http.get<WarehousesTables[]>(queryList);
+  }
+
+  getLatestItemsList(searchTerm: string): Observable<LatestItems[]> {
+    const params: string[] = [];
+    if (searchTerm) params.push(`SearchTerm=${encodeURIComponent(searchTerm)}`);
+    return this.http.get(`Item/GetLatestItemsList?${params}`);
   }
 }
