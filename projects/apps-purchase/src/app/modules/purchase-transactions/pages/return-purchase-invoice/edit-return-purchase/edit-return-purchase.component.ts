2bb28ae2691b7780130e37b6f3a702a7418f2256
Ahmed-Gamal-m <Ahmed.Gamal@microtec.com.eg>
2024-12-01 13:03:01 +0200

    return purchase invoice

---
+++ projects/apps-purchase/src/app/modules/purchase-transactions/pages/return-purchase-invoice/edit-return-purchase/edit-return-purchase.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-return-purchase',
+  templateUrl: './edit-return-purchase.component.html',
+  styleUrl: './edit-return-purchase.component.scss'
+})
+export class EditReturnPurchaseComponent {
+
+}
