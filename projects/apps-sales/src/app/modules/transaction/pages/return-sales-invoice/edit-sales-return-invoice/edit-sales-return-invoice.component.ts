b94679571953608644c2b60994f0341a49961c9c
Mahmoud Zaki <115453640+mahmoudzakimek@users.noreply.github.com>
2024-12-05 17:10:35 +0200

    push the ui of the sales return  add

---
+++ projects/apps-sales/src/app/modules/transaction/pages/return-sales-invoice/edit-sales-return-invoice/edit-sales-return-invoice.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-sales-return-invoice',
+  templateUrl: './edit-sales-return-invoice.component.html',
+  styleUrl: './edit-sales-return-invoice.component.scss'
+})
+export class EditSalesReturnInvoiceComponent {
+
+}
