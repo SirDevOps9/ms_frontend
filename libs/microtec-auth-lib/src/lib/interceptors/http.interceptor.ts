df477ff44cacbc995dbe502d54c3aed2bde3f1e1
Mohamed Fathy <m.fathy@microtec.com.eg>
2024-12-08 14:49:38 +0200

    initialize

--- libs/microtec-auth-lib/src/lib/interceptors/http.interceptor.ts
+++ libs/microtec-auth-lib/src/lib/interceptors/http.interceptor.ts
@@ -6,7 +6,7 @@ import {
   HttpEvent,
   HttpErrorResponse,
 } from '@angular/common/http';
-import { Observable, catchError, switchMap, throwError } from 'rxjs';
+import { Observable, Subject, catchError, switchMap, throwError, finalize } from 'rxjs';
 import { RouterService } from 'shared-lib';
 import { AuthService } from '../services';
 import { TokenModel } from '../types';
@@ -14,50 +14,95 @@ import { TokenModel } from '../types';
 @Injectable()
 export class ERPInterceptor implements HttpInterceptor {
   private isRefreshing = false;
+  private refreshTokenSubject: Subject<TokenModel> | null = null;
 
   constructor(private authService: AuthService, private routerService: RouterService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     return next.handle(request).pipe(
       catchError((error: any) => {
+        console.log('sss');
+
         if (error instanceof HttpErrorResponse) {
           if (error.status === 401) {
-           // console.log('401 Unauthorized Error - Attempting to refresh token');
             return this.handleUnAuthorizedError(request, next);
           } else if (error.status === 403) {
-            //console.log('403 Forbidden Error - Navigating to un-authorized page');
             this.routerService.navigateTo('un-authorized');
           }
         }
+        console.log('Int Error', error);
+
         return throwError(() => error);
       })
     );
   }
 
-  handleUnAuthorizedError(
+  private handleUnAuthorizedError(
     request: HttpRequest<unknown>,
     next: HttpHandler
   ): Observable<HttpEvent<unknown>> {
     if (!this.isRefreshing) {
       this.isRefreshing = true;
+      this.refreshTokenSubject = new Subject<TokenModel>();
+
       return this.authService.refreshToken().pipe(
         switchMap((data: TokenModel) => {
           this.authService.saveLoginData(data);
+          this.refreshTokenSubject!.next(data); // Notify waiting requests
+          this.refreshTokenSubject!.complete(); // Complete the subject
 
+          // Clone the request with the new token
           request = request.clone({
             setHeaders: { Authorization: `Bearer ${data.token}` },
           });
-          return next.handle(request);
+          return next.handle(request).pipe(
+            catchError((err) => {
+              console.log('UnAuth');
+
+              if (err instanceof HttpErrorResponse) {
+                if (err.status === 401) {
+                  this.authService.clearAllStorage();
+                  this.routerService.navigateTo('login');
+                }
+              }
+              return throwError(() => err);
+            })
+          );
         }),
-        catchError((err) => {
-          if (err.status == 401) {
-            this.authService.clearAllStorage();
-            this.routerService.navigateTo('login');
-          }
+        catchError((err: HttpErrorResponse) => {
+          // Handle refresh token failure
+          console.log('error',err);
+          this.authService.clearAllStorage();
+          this.routerService.navigateTo('login');
+          // Notify waiting requests that the refresh failed
+          this.refreshTokenSubject!.error(err);
           return throwError(() => err);
+        }),
+        finalize(() => {
+          this.isRefreshing = false;
         })
       );
     }
-    return next.handle(request);
+
+    // If a refresh is already in progress, wait for it to complete
+    return this.refreshTokenSubject!.pipe(
+      switchMap((token: TokenModel) => {
+        request = request.clone({
+          setHeaders: { Authorization: `Bearer ${token.token}` },
+        });
+        return next.handle(request).pipe(
+          catchError((err) => {
+            console.log('subject error',err);
+            if (err instanceof HttpErrorResponse) {
+              if (err.status === 401) {
+                this.authService.clearAllStorage();
+                this.routerService.navigateTo('login');
+              }
+            }
+            return throwError(() => err);
+          })
+        );
+      })
+    );
   }
 }
--- projects/apps-sales/src/environments/environment.development.ts
+++ projects/apps-sales/src/environments/environment.development.ts
@@ -2,13 +2,13 @@ import { IEnvironment } from 'shared-lib';
 
 export const environment: IEnvironment = {
   production: false,
-  baseUrl: 'http://localhost:2005',
+  baseUrl: 'http://localhost:5001/erp-apis',
   Version: 'V1',
   ClientId: 'Apps-Sales',
   Platform: 'Web',
   photoBaseUrl: 'https://intmicrotec.neat-url.com:2022',
   AuthConfiguration: {
-    authority: 'http://localhost:5001/auth-apis',
+    authority: 'https://localhost:7116',
     clientId: 'microtec_erp_frontend',
     redirectUrl: window.location.origin + '/erp/login-redirect',
     logoutRedirectUri: window.location.origin + '/erp/logout-redirect',
