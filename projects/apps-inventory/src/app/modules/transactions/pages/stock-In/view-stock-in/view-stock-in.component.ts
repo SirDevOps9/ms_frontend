750c0dfbafc9f082c485d056a154fef3d8f1844b
ahmed.saad@doctor-code.net <ahmed.msaad1516@gmail.com>
2024-12-10 15:22:40 +0200

    push

--- projects/apps-inventory/src/app/modules/transactions/pages/stock-In/view-stock-in/view-stock-in.component.ts
+++ projects/apps-inventory/src/app/modules/transactions/pages/stock-In/view-stock-in/view-stock-in.component.ts
@@ -77,6 +77,9 @@ export class ViewStockInComponent {
     }
   }
 
+
+  
+
   onSearchTermChange(search: any): void {
     const term = search.target.value?.toLowerCase() || '';
     this.filteredData = this.tableData.filter((item) => {
