c8bad6325406ad3afc2006336ae2748eb389421e
MuhammedNabil1 <Muhammed.Nabil@microtec.com.eg>
2024-12-15 17:14:46 +0200

    edits

--- projects/apps-inventory/src/app/modules/items/pages/item-definition/item-definition-uom/item-definition-uom.component.ts
+++ projects/apps-inventory/src/app/modules/items/pages/item-definition/item-definition-uom/item-definition-uom.component.ts
@@ -11,34 +11,32 @@ import { DialogService } from 'primeng/dynamicdialog';
 @Component({
   selector: 'app-item-definition-uom',
   templateUrl: './item-definition-uom.component.html',
-  styleUrls: ['./item-definition-uom.component.scss']
+  styleUrls: ['./item-definition-uom.component.scss'],
 })
 export class ItemDefinitionUomComponent implements OnInit, OnDestroy {
   itemUomForm: FormGroup;
   id: number;
-  unitUsagesName:[]
-  uerSubDomainModulesLookupData : { id: number;  name: string }[] = []
-  userSubDomainModulesLookupData : { id: number;  name: string }[] = []
+  unitUsagesName: [];
+  uerSubDomainModulesLookupData: { id: number; name: string }[] = [];
+  userSubDomainModulesLookupData: { id: number; name: string }[] = [];
   private subscription: Subscription;
-currentLang:string = ''
+  currentLang: string = '';
   constructor(
     private fb: FormBuilder,
     private route: ActivatedRoute,
     private itemService: ItemsService,
     private toaster: ToasterService,
     private dialog: DialogService,
-    public languageService: LanguageService,
+    public languageService: LanguageService
   ) {
     this.id = this.route.snapshot.params['id'];
     this.currentLang = this.languageService.getLang();
-
   }
 
   ngOnInit(): void {
     this.createFormUom();
     this.getDataUomById();
-    this.getAlluserSubDomainModules()
-
+    this.getAlluserSubDomainModules();
   }
 
   ngOnDestroy(): void {
@@ -46,14 +44,13 @@ currentLang:string = ''
       this.subscription.unsubscribe();
     }
   }
-  onChanged(selectedValue: any) {
-  }
+  onChanged(selectedValue: any) {}
 
   createFormUom() {
     this.itemUomForm = this.fb.group({
       itemId: [0],
       uomCategoryId: [0],
-      uoms: this.fb.array([])
+      uoms: this.fb.array([]),
     });
   }
 
@@ -62,131 +59,136 @@ currentLang:string = ''
   }
   nameBaseUnit: string = '';
   createUomFormGroup(item: any): FormGroup {
-    const initialUnitUsagesNames = this.userSubDomainModulesLookupData
-      .filter((element: any) => item.unitUsages.includes(element.id));
+    const initialUnitUsagesNames = this.userSubDomainModulesLookupData.filter((element: any) =>
+      item.unitUsages.includes(element.id)
+    );
 
     return this.fb.group({
       uomId: [item.uomId || ''],
       nameEn: [item.nameEn || ''],
-      nameAr:[item.nameAr || ''],
+      nameAr: [item.nameAr || ''],
       conversionRatio: [item.conversionRatio],
       isActive: [item.isActive || false],
       isBaseUnit: [item.isBaseUnit || false],
       shortName: [item.shortName || ''],
       unitUsages: [item.unitUsages || null],
-      unitUsagesName: [initialUnitUsagesNames]
+      unitUsagesName: [initialUnitUsagesNames],
     });
   }
 
   getUnitUsages(uom: AbstractControl): FormArray {
     return uom.get('unitUsages') as FormArray;
   }
-nameCategory:string=''
-getDataUomById() {
-  this.subscription = this.itemService.ItemGetItemUomByIdObs.subscribe(
-    (data: any) => {
-      if (data && data.uoms && Array.isArray(data.uoms)) {
-        if(this.currentLang ==='en')
-          this.nameCategory =data.uomCategoryNameEn
-         else
-          this.nameCategory =data.uomCategoryNameAr
-        this.itemUomForm.patchValue({
-          itemId: data.itemId,
-          uomCategoryId: data.uomCategoryId,
-          name: data.name
-        });
-            this.itemUomForm.updateValueAndValidity()
-        this.uoms.clear();
-        data.uoms.forEach((uom: any) => {
-          this.uoms.push(this.createUomFormGroup(uom));
-        });
-        if (this.uoms.length > 0) {
-          const firstUom = this.uoms.at(0);
-          if (firstUom) {
-            this.nameBaseUnit = this.currentLang === 'en'
-              ? firstUom.get('nameEn')?.value
-              : firstUom.get('nameAr')?.value;
+  nameCategory: string = '';
+  getDataUomById() {
+    this.subscription = this.itemService.ItemGetItemUomByIdObs.subscribe(
+      (data: any) => {
+        if (data && data.uoms && Array.isArray(data.uoms)) {
+          if (this.currentLang === 'en') this.nameCategory = data.uomCategoryNameEn;
+          else this.nameCategory = data.uomCategoryNameAr;
+          this.itemUomForm.patchValue({
+            itemId: data.itemId,
+            uomCategoryId: data.uomCategoryId,
+            name: data.name,
+          });
+          this.itemUomForm.updateValueAndValidity();
+          this.uoms.clear();
+          data.uoms.forEach((uom: any) => {
+            this.uoms.push(this.createUomFormGroup(uom));
+          });
+          if (this.uoms.length > 0) {
+            const firstUom = this.uoms.at(0);
+            if (firstUom) {
+              this.nameBaseUnit =
+                this.currentLang === 'en'
+                  ? firstUom.get('nameEn')?.value
+                  : firstUom.get('nameAr')?.value;
+            }
           }
         }
-
+      },
+      (error) => {
+        console.error('Error fetching UOM data:', error);
       }
-    },
-    (error) => {
-      console.error('Error fetching UOM data:', error);
-    }
-  );
+    );
 
-  this.itemService.getItemGetItemUomById(this.id);
-}
-
-  getAlluserSubDomainModules(){
-    this.itemService.getUserSubDomainModules()
-    this.itemService.userSubDomainModules.subscribe(res=>{
-this.userSubDomainModulesLookupData= res
-    })
-  }
-  accountSelected(event: any) {
-
+    this.itemService.getItemGetItemUomById(this.id);
   }
 
-  onEdit(data:any) {
+  getAlluserSubDomainModules() {
+    this.itemService.getUserSubDomainModules();
+    this.itemService.userSubDomainModules.subscribe((res) => {
+      this.userSubDomainModulesLookupData = res;
+    });
+  }
+  accountSelected(event: any) {}
+
+  onEdit(data: any) {
     const dialogRef = this.dialog.open(EditCategoryUomComponent, {
-        width: '800px',
-        height: '600px',
-        data: this.nameCategory
+      width: '800px',
+      height: '600px',
+      data: this.nameCategory,
     });
 
     dialogRef.onClose.subscribe((data) => {
-        if (data) {
-          this.itemUomForm.patchValue({ categoryId: this.nameCategory });
-   this.uoms.patchValue(data.uoMs)
-   this.nameBaseUnit = this.uoms.at(0).get('nameEn')?.value;
+      if (data) {
+        this.uoms.clear();
+         if (this.currentLang === 'en') {
+          this.nameCategory = data.uomCategoryNameEn;  
+         } else {
+          this.nameCategory = data.uomCategoryNameAr;
+          this.nameBaseUnit = data.nameEn
+         } 
 
-   if(this.currentLang ==='en'){
-    this.nameCategory =data.uomCategoryNameEn
-   }
-   else{
-    this.nameCategory =data.uomCategoryNameAr
+        console.log("heeey" , data)
+        let baseUnit = data?.uoMs.find((elem : any)=>elem.isBaseUnit == true)
+        let nonBaseUnit = data?.uoMs.filter((elem : any)=> !elem.isBaseUnit)
 
-   }
-this.itemUomForm.patchValue({
+         this.itemUomForm.patchValue({...baseUnit});
+         nonBaseUnit.forEach((item : any) => {
+          let itemFormGroup = this.fb.group({...item})
+            this.uoms.push(itemFormGroup)
+         });
+        
 
-  uomCategoryId: data.uomCategoryId,
-  name: data.name
-});
-this.itemUomForm.updateValueAndValidity()
-        } else {
-            this.getDataUomById();
-        }
+        // this.uoms.controls.forEach(item=>{
+        // })
+
+        // this.uoms.patchValue(data.uoMs);
+        // this.nameBaseUnit = this.uoms.at(0).get('nameEn')?.value;
+
+        // if (this.currentLang === 'en') {
+        //   this.nameCategory = data.uomCategoryNameEn;
+        // } else {
+        //   this.nameCategory = data.uomCategoryNameAr;
+        // }
+        // this.itemUomForm.patchValue({
+        //   uomCategoryId: data.uomCategoryId,
+        //   name: data.name,
+        // });
+        // this.itemUomForm.updateValueAndValidity();
+      } else {
+        this.getDataUomById();
+      }
     });
-}
-
+  }
 
   uomss: any[] = [];
 
   handleFetchedData(data: any) {
-
-      this.uomss = data.uoMs;
-      if(data)
-return data.uoMs;
+    this.uomss = data.uoMs;
+    if (data) return data.uoMs;
   }
 
   usercHN(e: any, fb: FormGroup) {
-
-    let data = this.userSubDomainModulesLookupData.filter((element: any) =>
-      e.includes(element.id)
-    );
+    let data = this.userSubDomainModulesLookupData.filter((element: any) => e.includes(element.id));
 
     fb.get('unitUsages')?.setValue(e);
     fb.get('unitUsagesName')?.setValue(data);
-
   }
 
   submit() {
-
-      const payload = this.itemUomForm.value;
-      this.itemService.updatetemGetItemUomById(payload)
-
-
+    const payload = this.itemUomForm.value;
+    this.itemService.updatetemGetItemUomById(payload);
   }
 }
