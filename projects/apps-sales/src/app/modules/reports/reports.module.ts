e78bf918757e3d65517937568c933aa64ca21dab
Bsihoy <beshoy@nbsventure.com>
2024-12-15 11:57:30 +0200

    page_structure

---
+++ projects/apps-sales/src/app/modules/reports/reports.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CustomerReportComponent } from './pages/customer-report/customer-report.component';
+
+
+
+@NgModule({
+  declarations: [
+    CustomerReportComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ReportsModule { }
